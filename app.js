6a781b7214018b84d6d37f19f3d9a92e42db72ad
Michael Zhang <failed.down@gmail.com>
2014-12-14 11:37:58 -0600

    new app

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var http = require("http");
+var api = require("./api/api");
+var router = require("./app/router");
+
+var app = express();
+
+app.configure(function() {
+	app.set("port", process.env.PORT || 3000);
+	app.set("views", __dirname + "/app/jade");
+	app.set("view engine", "jade");
+	app.locals.pretty = true;
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.methodOverride());
+	app.use(express.session({
+		secret: process.env.APP_SECRET,
+	}));
+
+	app.use(express.static(__dirname + "/web"));
+});
+
+api(app);
+router(app);
+
+http.createServer(app).listen(app.get("port"), function() {
+	console.log("[app.js] listening on port " + app.get("port") + "...");
+});
